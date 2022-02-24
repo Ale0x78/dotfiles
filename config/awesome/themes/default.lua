fc071ab1d1f579b610a5922280e58510e6da2a50
ale0x78 <anahape@ncsu.edu>
2022-02-24 01:54:02 -0500

    Moved the wallpaper location

--- config/awesome/themes/default.lua
+++ config/awesome/themes/default.lua
@@ -8,7 +8,7 @@ local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
-
+local home = gfs.get_xdg_config_home()
 local theme = {}
 
 theme.font          = "sans 8"
@@ -97,7 +97,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path.."default/background.png"
+theme.wallpaper = home .."/dotfiles/wallpapers/PO_W1.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
