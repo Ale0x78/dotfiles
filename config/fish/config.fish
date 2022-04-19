3a00c5b534ff9d79331d67dcf0a0b5ad192dd491
Alex <alex@Howl.local>
2022-04-19 13:16:30 -0400

    Universal Cargo Path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,8 +3,9 @@ if status is-interactive
 end
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.bin
-fish_add_path /Users/alex/.cargo/bin 
+fish_add_path $HOME/.cargo/bin 
 status --is-interactive; and rbenv init - fish | source
+alias ibrew='arch -x86_64 /usr/local/bin/brew'
 function fish_greeting
   fortune | cowsay -f flaming-sheep  
 end
